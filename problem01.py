6e294d1f745a77093c445d34fd36b63ab1b8b881
matthewwardle <MJW867@student.bham.ac.uk>
2023-09-16 22:13:07 +0100

    Problem 1 Solution

--- problem01.py
+++ problem01.py
@@ -7,4 +7,17 @@ we get 3, 5, 6, and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000.
 """
 
-print("Hello World!")
+def sumMultiplesOf(multiples,max):
+    sum = 0
+    for x in range(max):
+        if isMultipleOf(x, multiples):
+            sum += x
+    return sum
+
+def isMultipleOf(i, multiples):
+    for j in multiples:
+        if i%j == 0:
+            return True
+    return False
+
+print(sumMultiplesOf([3,5],1000))
